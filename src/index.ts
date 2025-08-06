939eca5a4f3b669cdb020964efc23cd368b84ac8
keval vyas <keval.vyas@spellenx.com>
2025-08-06 15:42:48 +0530

    Initiliaze commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// src/index.ts
+// import { add } from "#root/utils/math.js"; // Note the .js extension here!
+// import { greet } from "#root/components/greeting.js"; // Note the .js extension here!
+
+import calculation from "#utils";
+
+
+console.log(`5 - 3 = ${calculation.subtract(5, 3)}`);
---
+++ src/utils/math.ts
@@ -0,0 +1,12 @@
+class Calculation {
+    add(a: number, b: number): number {
+        return a + b;
+    }
+    subtract(a: number, b: number): number {
+        return a - b;
+    }
+}
+
+const calculation = new Calculation();
+
+export default calculation;
